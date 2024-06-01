e47837aad05472c4aa6b0fda1b17787e1034dee3
feolex <feolex@niuitmo.ru>
2024-06-01 15:20:59 +0300

    fix: add forgotten db name

--- DB_example/Common/scripts/patroniEnable.sh
+++ DB_example/Common/scripts/patroniEnable.sh
@@ -3,7 +3,7 @@ cd /patroni
 docker build -t patroni .
 docker compose up -d
 sleep 10
-PGPASSWORD="$POSTGRES_PASS" pg_dump -U "$POSTGRES_USER" -h localhost  > patroni_backup.sql
+PGPASSWORD="$POSTGRES_PASS" pg_dump -d "$POSTGRES_DB" -U "$POSTGRES_USER" -h localhost  > patroni_backup.sql
 docker exec -i demo-patroni1 psql -U postgres postgres < patroni_backup.sql
 docker exec -i demo-patroni2 psql -U postgres postgres < patroni_backup.sql
 docker exec -i demo-patroni3 psql -U postgres postgres < patroni_backup.sql
